5296df3aeddb817e6e40044ab5150699b4920faa
maximofernandezriera <maxfernandez@gmail.com>
2024-01-22 12:01:36 +0100

    Initial version.

--- src/main/java/edu/maximofernandez/SnailTests.java
+++ src/main/java/edu/maximofernandez/SnailTests.java
@@ -2,12 +2,10 @@ package edu.maximofernandez;
 
 import org.junit.Assert;
 import org.junit.Test;
-import java.util.Arrays;
-import static java.util.stream.Collectors.joining;
 
 public class SnailTests {
     @Test
-    public void SnailTest1() {
+    public void SnailBasicTests() {
         int[][] array
                 = {{1, 2, 3},
                 {4, 5, 6},
@@ -17,12 +15,37 @@ public class SnailTests {
     }
 
     public String int2dToString(int[][] a) {
-        return Arrays.stream(a).map(Arrays::toString).collect(joining("\n"));
+        StringBuilder sb = new StringBuilder();
+        for (int[] innerArray : a) {
+            for (int i : innerArray) {
+                sb.append(i).append(", ");
+            }
+            // Remove the trailing comma and space
+            if (!sb.isEmpty()) {
+                sb.setLength(sb.length() - 2);
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
     public void test(int[][] array, int[] result) {
-        String text = int2dToString(array) + " should be sorted to " + Arrays.toString(result);
+        String text = int2dToString(array) + " should be sorted to " + intArrayToString(result);
         System.out.println(text);
         Assert.assertArrayEquals(result, Snail.snail(array));
+    }
+
+    public String intArrayToString(int[] a) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i : a) {
+            sb.append(i).append(", ");
+        }
+        // Remove the trailing comma and space
+        if (sb.length() > 1) {
+            sb.setLength(sb.length() - 2);
+        }
+        sb.append("]");
+        return sb.toString();
     }
 }
